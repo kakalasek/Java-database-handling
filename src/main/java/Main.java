6faa77c74721a82a69811e557b1c71554d1f4674
kakalasek <honziccekkj@seznam.cz>
2024-01-31 10:30:02 +0100

    Init, Connection class, succesfully connected

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,31 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
         try{
-        DatabaseConnection connection = DatabaseConnection.CreateConnection("jdbc:oracle:thin:@localhost:1521:xepdb1", "PIPA", "pipa");
+            Properties prop = new Properties();
 
-        Connection conn = connection.Connect();
+            FileInputStream ip = new FileInputStream("src/main/resources/config.properties");
 
-        conn.close();
+            prop.load(ip);
+
+            DatabaseConnection connection = DatabaseConnection.CreateConnection(prop.getProperty("url"), prop.getProperty("username"), prop.getProperty("password"));
+
+            Connection conn = connection.Connect();
+
+            conn.close();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Houston, our SQL is F*cked up!\n" + e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Houston, there aint no damn file like that!\n" + e);
+        } catch (IOException e) {
+            throw new RuntimeException("Houston, the file is in some stupid ass format!" + e);
         }
-   }
+    }
 }
 
 
